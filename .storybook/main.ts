19f2b632538c4099505cb2d01ac51b5e3dd242ea
Andres <andres99.dev@gmail.com>
2023-12-28 11:59:11 +0100

    chore: update deps

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,20 +1,24 @@
 import type { StorybookConfig } from '@storybook/react-vite'
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
-    'storybook-addon-react-router-v6',
-  ],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {},
-  },
-  docs: {
-    autodocs: 'tag',
-  },
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-onboarding',
+        '@storybook/addon-interactions',
+        'storybook-addon-react-router-v6',
+    ],
+    framework: {
+        name: '@storybook/react-vite',
+        options: {},
+    },
+    docs: {
+        autodocs: 'tag',
+    },
+    viteFinal: async config => {
+        config.assetsInclude = ['/sb-preview/runtime.js']
+        return config
+    },
 }
 export default config
